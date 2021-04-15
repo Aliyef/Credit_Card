5971bfc316c20cb4cb0d81c5ecc6c43cf673288a
Aliyef <80645715+Aliyef@users.noreply.github.com>
2021-04-15 19:50:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// ---------------------C A R D 1------------------------
+    document.getElementById("card1").onclick=function(){
+
+    document.getElementById("bottomtext2").style.display="none"
+    document.getElementById("bottomtext3").style.display="none"
+
+    document.getElementById("bottomtext1").style.display="flex"
+//     break
+
+    document.getElementById("visa2").style.display="none"
+    document.getElementById("visa3").style.display="none"
+
+    document.getElementById("visa1").style.display="flex"
+   
+
+
+    
+
+
+}
+
+
+// ----------------------C A R D 2------------------------
+
+
+    document.getElementById("card2").onclick=function(){
+
+    document.getElementById("bottomtext1").style.display="none"
+    document.getElementById("bottomtext3").style.display="none"
+
+    document.getElementById("bottomtext2").style.display="flex"
+    //     break
+
+    document.getElementById("visa1").style.display="none"
+    document.getElementById("visa3").style.display="none"
+
+    document.getElementById("visa2").style.display="flex"
+    
+    
+    }
+
+
+// ----------------------C A R D 3------------------------
+
+
+    document.getElementById("card3").onclick=function(){
+
+    document.getElementById("bottomtext1").style.display="none"
+    document.getElementById("bottomtext2").style.display="none"
+
+    document.getElementById("bottomtext3").style.display="flex"
+        //     break
+
+    document.getElementById("visa1").style.display="none"
+    document.getElementById("visa2").style.display="none"
+
+    document.getElementById("visa3").style.display="flex"
+        
+        
+        }
